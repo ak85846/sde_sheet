07d9fad3aa3cbb56318e0916bf3f74541a36053c
ak85846 <108025008+ak85846@users.noreply.github.com>
2022-08-16 20:34:51 +0530

    Time: 2859 ms (5.00%), Space: 292.3 MB (8.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-int lengthOfLIS(vector<int>& nums) {
-    vector<int> res;
-    for(int i=0; i<nums.size(); i++) {
-        auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
-        if(it==res.end()) res.push_back(nums[i]);
-        else *it = nums[i];
+    
+    int fun(int indx,int prev_indx,vector<int>& nums,int n,vector<vector<int>> &dp){
+        if(indx==n)
+            return 0;
+        if(dp[indx][prev_indx+1]!=-1)
+            return dp[indx][prev_indx+1];
+        int len=0+fun(indx+1,prev_indx,nums,n,dp);
+        if(prev_indx==-1 || nums[indx]>nums[prev_indx]){
+            len=max(len,1+fun(indx+1,indx,nums,n,dp));
+        }
+        return dp[indx][prev_indx+1]=len;
     }
-    return res.size();
-}
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+            return fun(0,-1,nums,n,dp);
+    }
 };
